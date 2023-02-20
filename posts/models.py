147033051ad6eff385bb9dec5236855251ad48eb
kidsage <ruhyun92@gmail.com>
2023-02-20 17:19:44 +0900

    Feat: comment model 기능 추가
    + User field 추가
    + Parent field 추가 > reply 기능 구현 대비

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -35,10 +36,12 @@ class Tag(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField('CONTENT')
     created_at = models.DateTimeField('CREATED AT', auto_now_add=True)
     updated_at = models.DateTimeField('UPDATED AT', auto_now=True)
+    parent = models.ForeignKey('self' , null=True , blank=True , on_delete=models.CASCADE , related_name='replies')
 
     @property
     def short_content(self):
@@ -46,3 +49,16 @@ class Comment(models.Model):
 
     def __str__(self):
         return self.short_content
+
+    class Meta:
+        ordering=['-date_posted']
+
+    @property
+    def children(self):
+        return Comment.objects.filter(parent=self).reverse()
+
+    @property
+    def is_parent(self):
+        if self.parent is None:
+            return True
+        return False
