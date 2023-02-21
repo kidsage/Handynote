dc5c263abe06a27340cf7d525a9492d5f363f812
kidsage <ruhyun92@gmail.com>
2023-02-21 23:06:34 +0900

    Style: comment serializer field 변경
    + '__all__'에서 필요한 두 필드만 기입할 수 있도록 변경

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -11,7 +11,7 @@ class PostSerializer(serializers.ModelSerializer):
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['content', 'parent']
 
 
 class CategorySerializer(serializers.ModelSerializer):
