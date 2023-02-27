030a763dce1084351aeb1c04e2a87d6b3e186b08
kidsage <ruhyun92@gmail.com>
2023-02-27 17:51:19 +0900

    수정된 모델로 인한 serializer 추가 수정

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -5,7 +5,7 @@ from posts.models import *
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id', 'title', 'image', 'like', 'category']
+        fields = ['id', 'title', 'image', 'category']
 
 
 class CategorySerializer(serializers.ModelSerializer):
