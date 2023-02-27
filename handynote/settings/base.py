d3ebdb085967cbbeec334257acf2d3f6251da666
kidsage <ruhyun92@gmail.com>
2023-02-27 17:28:51 +0900

    drf 세팅 수정

--- handynote/settings/base.py
+++ handynote/settings/base.py
@@ -145,6 +145,9 @@ AUTH_USER_MODEL = 'users.User' # 기본 유저 모델 설정
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    # 'DEFAULT_PERMISSION_CLASSES': (
+    #     'rest_framework.permissions.IsAuthenticated'
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
