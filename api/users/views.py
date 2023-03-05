35c1aea78e2bb1ce75962172744a8beeaf043da2
kidsage <ruhyun92@gmail.com>
2023-03-06 01:26:10 +0900

    Refactor: login, logout 기능 리팩토링
    + modelviewset 내부에서 구현할 수 있도록 수정

--- api/users/views.py
+++ api/users/views.py
@@ -1,11 +1,16 @@
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import (
+    authenticate, 
+    login as auth_login, 
+    logout as auth_logout
+)
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from users.models import *
@@ -24,26 +29,16 @@ class UserViewSet(ModelViewSet):
             permission_classes = [IsAuthenticatedOrReadOnly]
         elif self.action == 'create' or self.action == 'retrieve':
             permission_classes = [AllowAny]
+        elif self.action == 'login':
+            permission_classes = [AllowAny]
+        elif self.action == 'logout':
+            permission_classes = [IsSelf]
         else:
             permission_classes = [IsSelf | IsAdminUser]
         return [permission() for permission in permission_classes]
     
-    def logout(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-        except KeyError:
-            return Response({"error": "refresh_token is required"}, status=status.HTTP_400_BAD_REQUEST)
-        
-        token = RefreshToken(refresh_token)
-        token.blacklist()
-
-        return Response({"success": "Logout success"}, status=status.HTTP_200_OK)
-    
-
-class LoginAPIView(APIView):
-    permission_classes = [AllowAny]
-
-    def post(self, request):
+    @action(detail=False, methods=['post'])
+    def login(self, request):
         email = request.data.get('email')
         password = request.data.get('password')
 
@@ -51,27 +46,33 @@ class LoginAPIView(APIView):
 
         if user is not None:
             refresh = RefreshToken.for_user(user)
-            return Response({
-                'access': str(refresh.access_token),
-                'refresh': str(refresh)
-            }, status=status.HTTP_200_OK)
+            req = {
+                # "user": User.objects.filter(email=email).values(),
+                "token": {
+                    "access": str(refresh.access_token),
+                    "refresh": str(refresh),
+                },
+                "message": "User Login Successful.",
+            }
+            auth_login(request, user)
+            return Response(req, status=status.HTTP_200_OK)
         else:
             return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-
-
-class LogoutAPIView(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request):
-        try:
-            refresh_token = request.data["refresh_token"]
-            token = RefreshToken(refresh_token)
-            token.blacklist()
-            logout(request)
-            return Response(status=status.HTTP_205_RESET_CONTENT)
-        except Exception as e:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=False, methods=['post'])
+    def logout(self, request):
+        refresh_token = request.data.get('refresh')
+        if refresh_token:
+            try:
+                token = RefreshToken(refresh_token)
+                token.blacklist()
+                # BlacklistToken.objects.create(token=refresh_token)
+                auth_logout(request)
+                return Response({'message': 'User Logged Out'}, status=status.HTTP_200_OK)
+            except (InvalidToken, TokenError):
+                return Response({'error': 'Invalid Token'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Refresh Token not found'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # user data를 넣어서 지원할 수 있도록 세팅할 예정
