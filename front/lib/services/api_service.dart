ad87202de2bba3c0bb9c3538d489fddaf7eb35f6
kidsage <ruhyun92@gmail.com>
2023-03-15 16:11:19 +0900

    Feat: Api CRUD 구현

--- front/lib/services/api_service.dart
+++ front/lib/services/api_service.dart
@@ -2,10 +2,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:front/models/handynote_model.dart';
 
-class ApiService {
+class HandynoteApi {
   static const String baseUrl = 'http://127.0.0.1:8000/api/post';
 
-  static Future<List<HandynoteModel>> getHandynoteList() async {
+  // Read(GET)
+  static Future<List<HandynoteModel>> getMemo() async {
     List<HandynoteModel> noteInstances = [];
     final url = Uri.parse(baseUrl);
     final response = await http.get(url);
@@ -18,5 +19,34 @@ class ApiService {
       return noteInstances;
     }
     throw Error();
+  }
+
+  // Create(Post)
+  static Future<http.Response> createMemo(
+      String title, user, category, content) async {
+    final response = await http.post(Uri.parse(baseUrl), body: {
+      'title': title,
+      'user': user,
+      'category': category,
+      'content': content,
+    });
+    return response;
+  }
+
+  // Update(Patch)
+  static Future<http.Response> updateMemo(
+      int id, String title, category, content) async {
+    final response = await http.patch(Uri.parse('$baseUrl/$id/'), body: {
+      'title': title,
+      'category': category,
+      'content': content,
+    });
+    return response;
+  }
+
+  // Delete
+  static Future<http.Response> deleteMemo(int id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/$id/'));
+    return response;
   }
 }
