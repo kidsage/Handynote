1501a9603889d881420533de24cd29eedd2b3900
kidsage <ruhyun92@gmail.com>
2023-04-04 22:30:02 +0900

    리팩토링으로 인한 클래스 이름 변경

--- front/test/widget_test.dart
+++ front/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:front/screens/home_screen.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyHomeScreen());
+    await tester.pumpWidget(const MyHomeScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
