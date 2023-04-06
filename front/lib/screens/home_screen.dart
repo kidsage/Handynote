eef54580d5c6f8e69c8ee9572013ea5664af6975
kidsage <ruhyun92@gmail.com>
2023-04-07 03:50:51 +0900

    Feat: 1st finish to home screen

--- front/lib/screens/home_screen.dart
+++ front/lib/screens/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:front/models/handynote_model.dart';
 import 'package:front/screens/detail_screen.dart';
+import 'package:front/screens/search_screen.dart';
 import 'package:front/services/api_service.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:front/widgets/handynote_widget.dart';
 
 class MyHomeScreen extends StatefulWidget {
   const MyHomeScreen({super.key});
@@ -10,33 +14,46 @@ class MyHomeScreen extends StatefulWidget {
 }
 
 class _MyHomeScreenState extends State<MyHomeScreen> {
-  List<dynamic> _memoList = [];
+  List<Note> _noteList = [];
+
+  int count = 0;
+  int axisCount = 0;
 
   @override
   void initState() {
     super.initState();
-    loadMemos();
+    loadNotes();
   }
 
-  Future<void> loadMemos() async {
-    final memoList = await HandynoteApi.getMemos();
+  Future<void> loadNotes() async {
+    final List<Note> noteList = await HandynoteApi.getNotes();
     setState(() {
-      _memoList = memoList;
+      _noteList = noteList;
+      count = _noteList.length;
     });
   }
 
-  void _showMemoDetail(int? id) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (BuildContext context) {
-          return DetailScreen(
-            id: id,
-          );
-        },
-      ),
-    ).then((_) {
-      loadMemos();
+  void navigateToDetail(String title, Note note) async {
+    bool result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => NoteDetail(
+                  appBarTitle: title,
+                  note: note,
+                )));
+
+    if (result == true) {
+      updateListView();
+    }
+  }
+
+  void updateListView() {
+    Future<List<Note>> noteListFuture = HandynoteApi.getNotes();
+    noteListFuture.then((noteList) {
+      setState(() {
+        _noteList = noteList;
+        count = _noteList.length;
+      });
     });
   }
 
@@ -44,61 +61,244 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Memo List'),
-      ),
-      body: ListView.builder(
-        itemCount: _memoList.length,
-        itemBuilder: (BuildContext context, int index) {
-          var memo = _memoList[index];
-          return ListTile(
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  memo.title,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  overflow: TextOverflow.visible,
+        title: Text('Notes', style: Theme.of(context).textTheme.headline5),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: _noteList.isEmpty
+            ? Container()
+            : IconButton(
+                onPressed: () async {
+                  final Note? result = await showSearch(
+                    context: context,
+                    delegate: NotesSearch(notes: _noteList),
+                  );
+                  navigateToDetail("Edit Note", result!);
+                },
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.black,
                 ),
-              ],
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'updated: ${memo.update.substring(2, 10)}',
-                    style: const TextStyle(
-                      fontSize: 13,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  Text(
-                    memo.content,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    overflow: TextOverflow.visible,
-                  ),
-                ],
               ),
-            ),
-            onTap: () {
-              _showMemoDetail(memo.id);
-            },
-          );
-        },
+        actions: [
+          _noteList.isEmpty
+              ? Container()
+              : IconButton(
+                  onPressed: () {
+                    setState(() {
+                      axisCount = axisCount == 2 ? 4 : 2;
+                    });
+                  },
+                  icon: Icon(
+                    color: Colors.black,
+                    axisCount == 2 ? Icons.list : Icons.grid_on,
+                  ),
+                )
+        ],
       ),
+      body: _noteList.isEmpty
+          ? Container(
+              color: Colors.white,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Click on the add button to add a new note!',
+                    style: Theme.of(context).textTheme.bodyText2,
+                  ),
+                ),
+              ),
+            )
+          : Container(
+              color: Colors.white,
+              child: getNotesList(),
+            ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _showMemoDetail(null);
+          navigateToDetail(
+            'Add Note',
+            Note(
+              id: 1000,
+              title: '',
+              content: '',
+              category: '',
+              update: '',
+              priority: 3,
+              color: 0,
+            ),
+          );
         },
-        child: const Icon(Icons.add),
+        tooltip: 'Add Note',
+        shape: const CircleBorder(
+            side: BorderSide(color: Colors.black, width: 2.0)),
+        backgroundColor: Colors.white,
+        child: const Icon(Icons.add, color: Colors.black),
       ),
     );
   }
+
+  Widget getNotesList() {
+    return MasonryGridView.count(
+      padding: const EdgeInsets.all(8),
+      crossAxisCount: 2,
+      mainAxisSpacing: 8,
+      crossAxisSpacing: 8,
+      itemCount: _noteList.length,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () {
+            navigateToDetail('Edit Note', _noteList[index]);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                  color: colors[_noteList[index].color],
+                  border: Border.all(width: 2, color: Colors.black),
+                  borderRadius: BorderRadius.circular(8.0)),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            _noteList[index].title,
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        getPriorityText(_noteList[index].priority),
+                        style: TextStyle(
+                          color: getPriorityColor(_noteList[index].priority),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            _noteList[index].content,
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        _noteList[index].update,
+                        style: Theme.of(context).textTheme.subtitle2,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  // Returns the priority color
+  Color getPriorityColor(int priority) {
+    switch (priority) {
+      case 1:
+        return Colors.red;
+      case 2:
+        return Colors.yellow;
+      case 3:
+        return Colors.green;
+      default:
+        return Colors.yellow;
+    }
+  }
+
+  // Returns the priority icon
+  String getPriorityText(int priority) {
+    switch (priority) {
+      case 1:
+        return '!!!';
+      case 2:
+        return '!!';
+      case 3:
+        return '!';
+      default:
+        return '!';
+    }
+  }
 }
+
+// API test code.
+// @override
+// Widget build(BuildContext context) {
+//   return Scaffold(
+//     appBar: AppBar(
+//       title: const Text('Note List'),
+//     ),
+//     body: ListView.builder(
+//       itemCount: _noteList.length,
+//       itemBuilder: (BuildContext context, int index) {
+//         var Note = _noteList[index];
+//         return ListTile(
+//           title: Column(
+//             crossAxisAlignment: CrossAxisAlignment.stretch,
+//             children: [
+//               Text(
+//                 Note.title,
+//                 style: const TextStyle(
+//                   fontSize: 25,
+//                   fontWeight: FontWeight.w500,
+//                 ),
+//                 overflow: TextOverflow.visible,
+//               ),
+//             ],
+//           ),
+//           subtitle: Padding(
+//             padding: const EdgeInsets.symmetric(vertical: 15),
+//             child: Column(
+//               crossAxisAlignment: CrossAxisAlignment.stretch,
+//               children: [
+//                 Text(
+//                   'updated: ${Note.update.substring(2, 10)}',
+//                   style: const TextStyle(
+//                     fontSize: 13,
+//                     fontWeight: FontWeight.w400,
+//                   ),
+//                 ),
+//                 Text(
+//                   Note.content,
+//                   style: const TextStyle(
+//                     fontSize: 20,
+//                     fontWeight: FontWeight.w500,
+//                   ),
+//                   overflow: TextOverflow.visible,
+//                 ),
+//               ],
+//             ),
+//           ),
+//           onTap: () {
+//             _showNoteDetail(Note.id);
+//           },
+//         );
+//       },
+//     ),
+//     floatingActionButton: FloatingActionButton(
+//       onPressed: () {
+//         _showNoteDetail(null);
+//       },
+//       child: const Icon(Icons.add),
+//     ),
+//   );
+// }
