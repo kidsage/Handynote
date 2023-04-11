b68767136e60f1099f9e5b89326df857ef7ead33
kidsage <ruhyun92@gmail.com>
2023-04-11 16:51:11 +0900

    edit to api_service

--- front/lib/services/api_service.dart
+++ front/lib/services/api_service.dart
@@ -7,8 +7,8 @@ class HandynoteApi {
   static const String baseUrl = 'http://127.0.0.1:8000/api/post';
 
   // Read(GET)
-  static Future<List<HandynoteModel>> getMemos() async {
-    List<HandynoteModel> noteInstances = [];
+  static Future<List<Note>> getNotes() async {
+    List<Note> noteInstances = [];
     final url = Uri.parse(baseUrl);
     final response = await http.get(url);
     if (response.statusCode == 200) {
@@ -16,7 +16,7 @@ class HandynoteApi {
           jsonDecode(utf8.decode(response.bodyBytes));
       final List<dynamic> notes = map['results'];
       for (var note in notes) {
-        noteInstances.add(HandynoteModel.fromJson(note));
+        noteInstances.add(Note.fromJson(note));
       }
       return noteInstances;
     }
@@ -24,37 +24,40 @@ class HandynoteApi {
   }
 
   // Create(Post)
-  static Future<http.Response> createMemo(
-      String title, category, content) async {
+  static Future<http.Response> createNote(
+      String title, category, content, int priority, color) async {
     final response = await http.post(
       Uri.parse('$baseUrl/'),
       body: {
         'title': title,
         'category': category,
         'content': content,
+        'priority': priority,
+        'color': color
       },
     );
     return response;
   }
 
   // Update(Patch)
-  static Future<http.Response> updateMemo(
-      int id, String title, category, content) async {
+  static Future<http.Response> updateNote(
+      int id, priority, color, String title, category, content) async {
     final response = await http.patch(
       Uri.parse('$baseUrl/$id/'),
       headers: {HttpHeaders.contentTypeHeader: 'application/json'},
       body: jsonEncode({
         'title': title,
-        // 'category': category,
+        'category': category,
         'content': content,
+        'priority': priority,
+        'color': color
       }),
     );
-    // return response;
     return response;
   }
 
   // Delete
-  static Future<http.Response> deleteMemo(int id) async {
+  static Future<http.Response> deleteNote(int id) async {
     final response = await http.delete(Uri.parse('$baseUrl/$id/'));
     return response;
   }
