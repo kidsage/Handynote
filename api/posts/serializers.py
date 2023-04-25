55bc5fe37b45b0a11911cc68b9dcda72150d2639
kidsage <ruhyun92@gmail.com>
2023-04-25 22:45:14 +0900

    Fix: edit category method
    + id 대신 name으로 serializer가 작동할 수 있도록 변경

--- api/posts/serializers.py
+++ api/posts/serializers.py
@@ -3,9 +3,16 @@ from posts.models import *
 
 
 class PostSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()
+    updated_at = serializers.SerializerMethodField()
+
     class Meta:
         model = Post
         fields = ['id', 'title', 'category', 'content', 'priority', 'color', 'updated_at']
+        read_only_fields = ('updated_at',)
+
+    def get_updated_at(self, obj):
+        return obj.updated_at.strftime('%Y-%m-%d %H:%M:%S')
 
 
 class CategorySerializer(serializers.ModelSerializer):
