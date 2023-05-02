13426cd29664ab300e64913e11bdfdd933b63c78
kidsage <ruhyun92@gmail.com>
2023-05-03 03:24:23 +0900

    change logging level

--- handynote/settings/dev.py
+++ handynote/settings/dev.py
@@ -56,12 +56,12 @@ LOGGING = {
     },
     "root": {
         "handlers": ["console"],
-        "level": "DEBUG",
+        "level": "WARNING",
     },
     "loggers": {
         "django": {
             "handlers": ["console"],
-            "level": os.getenv("DJANGO_LOG_LEVEL", "DEBUG"),
+            "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
             "propagate": False,
         },
     },
